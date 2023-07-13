2d93e4ccc89227112b6b71a8807d8f050623bd8a
Fernando Da Silva <82740676+fernando4capital@users.noreply.github.com>
2023-07-13 07:05:07 -0400

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,22 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { expenses, Location } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += (item.unitprice * item.quantity));
+    }, 0);
+
+    return (
+        <input
+        required='required'
+        type='number'
+        id='cost'
+        value={quantity}
+        style={{size: 10}}
+        onChange={(event) => setQuantity(event.target.value)}>
+        </input>
+    );
+};
+
+export default Budget;
