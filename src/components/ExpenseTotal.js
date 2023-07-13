795bca556e0f294aff7ab6c70407722d05a98c23
Fernando Da Silva <82740676+fernando4capital@users.noreply.github.com>
2023-07-13 06:46:26 -0400

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,1 +1,17 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Expenses = () => {
+    const { expenses, Location } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += (item.unitprice * item.quantity));
+    }, 0);
+
+    return (
+        <div className='alert alert-primary'>
+            <span>Spent so far: {Location}{totalExpenses}</span>
+        </div>
+    );
+};
+
+export default Expenses;
